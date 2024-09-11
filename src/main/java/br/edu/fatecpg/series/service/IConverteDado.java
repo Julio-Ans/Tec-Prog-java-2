085e84f66e360192499b403aa209de9da184b5e7
Julio-Ans <160240999+Julio-Ans@users.noreply.github.com>
2024-09-11 16:13:16 -0300

    Delete src directory

--- src/main/java/br/edu/fatecpg/series/service/IConverteDado.java
+++
@@ -1,8 +0,0 @@
-package br.edu.fatecpg.series.service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public interface IConverteDado {
-    <T> T obterDado(String json, Class<T> classe) throws JsonProcessingException;
-
-}
